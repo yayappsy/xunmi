1952f59453a5d738068cc52546a3da02aa166acb
chenwenlong <942057398@qq.com>
2020-04-12 15:06:41 +0800

    runtime删除

--- runtime/cache/b6/2c1e2a759fad26f36b6705aeb096e7.php
+++
@@ -1,4 +0,0 @@
-<?php
-//000000086400
- exit();?>
-a:3:{i:0;a:5:{s:2:"id";i:121;s:4:"info";s:39:"CRMEB电商系统 V 3.0 即将上线！";s:3:"url";s:22:"/pages/news_list/index";s:4:"show";s:1:"2";s:7:"wap_url";s:10:"/news_list";}i:1;a:5:{s:2:"id";i:122;s:4:"info";s:39:"CRMEB电商系统 V 3.0 即将上线！";s:3:"url";s:22:"/pages/news_list/index";s:4:"show";s:1:"2";s:7:"wap_url";s:10:"/news_list";}i:2;a:5:{s:2:"id";i:123;s:4:"info";s:39:"CRMEB电商系统 V 3.0 即将上线！";s:3:"url";s:22:"/pages/news_list/index";s:4:"show";s:1:"2";s:7:"wap_url";s:10:"/news_list";}}

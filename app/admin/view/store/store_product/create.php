30333785f7dc8532627565287f8a4ad1f912f945
ChesonHuang <1348641216@qq.com>
2020-04-24 21:22:47 +0800

    update create.php

--- app/admin/view/store/store_product/create.php
+++ app/admin/view/store/store_product/create.php
@@ -1580,7 +1580,7 @@
                 that.getRuleList(1);
             }
             $(that.$refs.filElem).change(function () {
-                var inputFile = this.$refs.filElem.files[0];
+                var inputFile = that.$refs.filElem.files[0];
                 that.requestPost(that.U({c:"widget.video",a:'get_signature'})).then(function (res) {
                     AdminUpload.upload(res.data.uploadType,{
                         token: res.data.uploadToken || '',
